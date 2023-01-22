9b8ac2260210516b8f9c92fe612998d6f6b2cb30
Frans de Waal <fmdewaal@gmail.com>
2023-01-23 00:37:22 +0200

    Added a power metric

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,6 +1,7 @@
 import { createMemo, For } from 'solid-js';
 import PokeImage from './PokeImage.jsx';
 import TypeLabel from './TypeLabel.jsx';
+import TYPES, { MATCHUPS } from '../../assets/types';
 
 export default function Pokemon(props) {
   const sortedTypes = createMemo(() => props.types.sort((typeA, typeB) => {
@@ -12,6 +13,23 @@ export default function Pokemon(props) {
     }
     return 0;
   }));
+
+  const offence = createMemo(() => {
+    const offenceValue = props.types.reduce((total, pokemonType) => {
+      const typeMatchups = Object.values(MATCHUPS[pokemonType]);
+      return total + typeMatchups.reduce((subTotal, matchupValue) => subTotal + matchupValue, 0);
+    }, 0) / Object.keys(TYPES).length;
+    return ((offenceValue - 0.8) * 100).toFixed(0);
+  });
+
+  const defence = createMemo(() => {
+    const defenceValue = props.types.reduce((total, pokemonType) => {
+      const allTypes = Object.values(TYPES);
+
+      return total + allTypes.reduce((subTotal, type) => subTotal + MATCHUPS[type][pokemonType], 0);
+    }, 0) / Object.keys(TYPES).length;
+    return ((1.2 - defenceValue) * 100).toFixed(0);
+  });
 
   return (
     <div
@@ -27,10 +45,15 @@ export default function Pokemon(props) {
       "
       onClick={() => props.onClick()}
     >
-      <h3 class="flex flex-row flex-wrap items-center text-xl">
+      <div class="flex flex-row items-center">
         <PokeImage id={props.id} size="md" />
-        <span class="ml-3">{props.name}</span>
-      </h3>
+        <span class="flex-1 ml-3 text-xl">{props.name}</span>
+        <span class="text-xs text-red-dark">{offence()}</span>
+        <span class="text-xs text-black">
+          &nbsp;-&nbsp;
+        </span>
+        <span class="text-xs text-blue-dark">{defence()}</span>
+      </div>
       <div class="flex flex-row">
         <For each={sortedTypes()}>{(type) => <TypeLabel type={type} />}</For>
       </div>
