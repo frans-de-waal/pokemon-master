76df7a1cb4788d7a93e12771a80671fade830ab0
Frans de Waal <fmdewaal@gmail.com>
2023-01-22 23:13:28 +0200

    Added Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import Party from './components/Party.jsx';
 import PokeList from './components/PokeList.jsx';
+import Header from './components/Header.jsx';
 
 function App() {
   return (
     <div class="bg-white min-h-screen max-h-screen font-poke flex flex-col">
-      <header class="bg-blue-dark">
-        <h1 class="text-4xl text-yellow-dark py-5 px-8">
-          Pokemon Master
-        </h1>
-      </header>
+      <Header />
       <main class="flex-1 flex flex-row items-stretch">
         <Party />
         <PokeList />
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header class="bg-blue-dark">
+      <h1 class="text-4xl text-yellow-dark py-5 px-8">
+        Pokemon Master
+      </h1>
+    </header>
+  );
+}
